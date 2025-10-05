60659580edb5b7a9fe5d67d0431bc662c5cca78c
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2025-10-05 16:27:04 +0300

    - hyprland

--- hyprland.sh
+++
@@ -1,5 +0,0 @@
-sudo pacman -Syu --needed hypridle hyprland hyprlang hyprlock hyprpaper mako waybar hyprpolkitagent nautilus rofi-wayland hyprshot blueman wlogout hyprpicker otf-font-awesome ttf-jetbrains-mono-nerd ly gnome-keyring xdg-desktop-portal-hyprland xdg-desktop-portal-gtk darkman
-sudo systemctl enable ly
-git clone --depth 1 https://github.com/MAnitosik/my-linux.git
-cd my-linux/
-bash install.sh
