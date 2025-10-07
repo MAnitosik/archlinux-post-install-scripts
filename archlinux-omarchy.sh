e20cd6876c5593ebc17f6d8c2055719691f7fffd
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2025-10-07 22:55:53 +0300

    omarchy

---
+++ archlinux-omarchy.sh
@@ -0,0 +1,10 @@
+sudo ufw enable
+curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
+tar xvf cachyos-repo.tar.xz && cd cachyos-repo
+sudo ./cachyos-repo.sh
+sudo pacman -S scx-scheds-git
+sudo systemctl enable --now scx_loader.service
+sudo pacman -S sbctl
+sudo sbctl create-keys
+sudo sbctl enroll-keys --microsoft
+curl -fsSL https://raw.githubusercontent.com/CachyOS/CachyOS-Settings/refs/heads/master/usr/bin/sbctl-batch-sign | sudo bash
---
+++ sbctl-batch-sign.sh
@@ -0,0 +1,1 @@
+curl -fsSL https://raw.githubusercontent.com/CachyOS/CachyOS-Settings/refs/heads/master/usr/bin/sbctl-batch-sign | sudo bash
