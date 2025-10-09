7b99b0a42949ce1a0349ef9cfae427a9251a4f61
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2025-10-09 21:51:20 +0300

    prefs

--- archlinux-omarchy.sh
+++ archlinux-omarchy.sh
@@ -1,3 +1,4 @@
+sudo pacman -Rns obsidian 1password-beta 1password-cli spotify lazygit lazydocker typora docker docker-compose github-cli libreoffice-fresh kdenlive obs-studio
 curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz && cd cachyos-repo
 sudo ./cachyos-repo.sh
@@ -7,3 +8,6 @@ sudo pacman -S sbctl
 sudo sbctl create-keys
 sudo sbctl enroll-keys --microsoft
 curl -fsSL https://raw.githubusercontent.com/CachyOS/CachyOS-Settings/refs/heads/master/usr/bin/sbctl-batch-sign | sudo bash
+
+sudo pacman -S onlyoffice-bin obs-studio-browser torbrowser-launcher
+yay -S amneziavpn-bin
