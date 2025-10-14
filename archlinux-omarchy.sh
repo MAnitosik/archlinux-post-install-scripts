9ad2e86343c7a0c0eec533080452c26acf4770d4
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2025-10-14 22:06:20 +0300

    Update archlinux-omarchy.sh

--- archlinux-omarchy.sh
+++ archlinux-omarchy.sh
@@ -2,12 +2,12 @@ sudo pacman -Rns lazygit lazydocker obsidian libreoffice-fresh obs-studio kdenli
 curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz && cd cachyos-repo
 sudo ./cachyos-repo.sh
-sudo pacman -S scx-scheds-git
-sudo systemctl enable --now scx_loader.service
 sudo pacman -S sbctl
 sudo sbctl create-keys
 sudo sbctl enroll-keys --microsoft
 curl -fsSL https://raw.githubusercontent.com/CachyOS/CachyOS-Settings/refs/heads/master/usr/bin/sbctl-batch-sign | sudo bash
+sudo pacman -S scx-scheds-git
+sudo systemctl enable --now scx_loader.service
 
 sudo pacman -S onlyoffice-bin obs-studio-browser torbrowser-launcher github-desktop fragments lact ventoy-bin gnome-boxes
 yay -S amneziavpn-bin
