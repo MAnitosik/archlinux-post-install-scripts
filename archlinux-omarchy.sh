f416d9e22759b127d2bbbdb34c685e8561beacde
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2025-10-18 02:13:25 +0300

    archlinux

--- archlinux-omarchy.sh
+++ archlinux-omarchy.sh
@@ -1,4 +1,5 @@
-sudo pacman -Rns lazygit lazydocker obsidian libreoffice-fresh obs-studio kdenlive 1password-beta 1password-cli typora spotify docker docker-compose github-cli
+sudo pacman -Rns lazygit lazydocker obsidian libreoffice-fresh signal-desktop obs-studio kdenlive 1password-beta 1password-cli typora spotify docker docker-compose github-cli
+
 curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz && cd cachyos-repo
 sudo ./cachyos-repo.sh
@@ -9,6 +10,7 @@ curl -fsSL https://raw.githubusercontent.com/CachyOS/CachyOS-Settings/refs/heads
 sudo pacman -S scx-scheds-git
 sudo systemctl enable --now scx_loader.service
 
-sudo pacman -S zed onlyoffice-bin obs-studio-browser torbrowser-launcher github-desktop fragments lact ventoy-bin gnome-boxes
+sudo pacman -S zed onlyoffice-bin telegram-desktop obs-studio-browser github-desktop
+sudo pacman -S torbrowser-launcher fragments lact ventoy-bin gnome-boxes gnome-disk-utility
 yay -S amneziavpn-bin
 sudo systemctl enable --now lactd
--- cachyos-clean.sh
+++
@@ -1,9 +0,0 @@
-sudo pacman -Syu --needed sbctl appmenu-gtk-module libdbusmenu-glib apparmor apparmor.d-git scx-scheds-git librewolf-bin torbrowser-launcher zed fragments obs-studio-browser showtime lact gparted loupe resources gnome-boxes ventoy-bin telegram-desktop github-desktop onlyoffice-bin flatpak
-sudo pacman -Syu --needed --asdeps wl-clipboard
-paru -Syu --needed goofcord-bin amneziavpn-bin pinta
-tldr --update
-sudo sbctl create-keys
-sudo sbctl enroll-keys --microsoft
-sudo sbctl-batch-sign
-sudo sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
-sudo systemctl enable --now apparmor lactd
