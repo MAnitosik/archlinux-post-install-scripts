274137706dda9b7081ad1e29f25f4529e1d824c9
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2025-10-23 19:23:25 +0300

    fixoooo

--- post-install.sh
+++ post-install.sh
@@ -4,7 +4,7 @@ curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz && cd cachyos-repo
 sudo ./cachyos-repo.sh
 
-sudo pacman -S sbctl
+sudo pacman -S --noconfirm --needed sbctl
 sudo sbctl create-keys
 sudo sbctl enroll-keys --microsoft
 curl -fsSL https://raw.githubusercontent.com/CachyOS/CachyOS-Settings/refs/heads/master/usr/bin/sbctl-batch-sign | sudo bash
