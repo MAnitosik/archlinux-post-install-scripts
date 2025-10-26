f27b8f135872fbc007760b2db6e2aa10d64e5c94
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2025-10-27 01:33:23 +0300

    network

--- post-install.sh
+++ post-install.sh
@@ -26,11 +26,13 @@ sudo mkdir /etc/scx_loader
 curl -fsSL https://raw.githubusercontent.com/MAnitosik/archlinux-post-install-scripts/refs/heads/main/etc/scx_loader/config.toml | sudo tee /etc/scx_loader/config.toml
 sudo systemctl enable --now scx_loader.service
 
+sudo modprobe tcp_bbr
 sudo pacman -S --noconfirm --needed zed onlyoffice-bin telegram-desktop obs-studio-browser
 sudo pacman -S --noconfirm --needed github-desktop torbrowser-launcher fragments lact ventoy-bin gnome-boxes exfatprogs
 yay -S --noconfirm --needed amneziavpn-bin
 sudo systemctl enable --now lactd
 curl -fsSL https://raw.githubusercontent.com/MAnitosik/archlinux-post-install-scripts/refs/heads/main/etc/hosts | sudo tee /etc/hosts
+curl -fsSL https://raw.githubusercontent.com/MAnitosik/archlinux-post-install-scripts/refs/heads/main/etc/sysctl.d/99-sysctl.conf | sudo tee /etc/sysctl.d/99-sysctl.conf
 curl -fsSL https://raw.githubusercontent.com/MAnitosik/archlinux-post-install-scripts/refs/heads/main/etc/systemd/resolved.conf.d/dnssec.conf | sudo tee /etc/systemd/resolved.conf.d/dnssec.conf
 
 curl -fsSL https://raw.githubusercontent.com/MAnitosik/archlinux-post-install-scripts/refs/heads/main/hypr/bindings.conf | tee ~/.config/hypr/bindings.conf
