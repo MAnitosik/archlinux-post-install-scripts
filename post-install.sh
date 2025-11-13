53008ed0baafcc3c21b421ecf43a272c182c9992
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2025-11-13 17:05:06 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -20,7 +20,7 @@ curl -fsSL https://raw.githubusercontent.com/MAnitosik/archlinux-post-install-sc
 
 curl -fsSL https://raw.githubusercontent.com/MAnitosik/archlinux-post-install-scripts/refs/heads/main/etc/sysctl.d/99-splitlock.conf | sudo tee /etc/sysctl.d/99-splitlock.conf
 
-sudo pacman -S --noconfirm --needed scx-scheds
+sudo pacman -S --noconfirm --needed scx-scheds scx-tools
 sudo mkdir /etc/scx_loader
 curl -fsSL https://raw.githubusercontent.com/MAnitosik/archlinux-post-install-scripts/refs/heads/main/etc/scx_loader/config.toml | sudo tee /etc/scx_loader/config.toml
 sudo systemctl enable --now scx_loader.service
