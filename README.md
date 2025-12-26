2e9307a016b17867f8a4f4a291a6df3539f82b6c
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2025-12-26 23:14:06 +0300

    REWORK

---
+++ README.md
@@ -0,0 +1,15 @@
+# archinstall requirements
+- UKI
+- EFI stub
+- Xorg + graphics drivers (profile)
+- multilib
+- x86-64-v3 capable CPU
+# sequence of scripts
+- zapret.sh (optional)
+- omarchy.sh/omarchy-2.sh
+- requirements.sh
+- reboot
+- post-install.sh
+- reboot
+# omarchy requirements
+- DHCP
--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -8,11 +8,11 @@ sudo mkdir /etc/udev/rules.d
 # changing channels for pacman (adding V3 packages), deleting some base omarchy packages and installing flatpak
 # lazygit and lazydocker are not needed, obsidian is just a smart note taking app, localsend is useless for me
 # 1password, typora and spotify are cringe
-omarchy-channel-set edge
 yay -S --needed alhp-keyring alhp-mirrorlist
 curl -fsSL https://raw.githubusercontent.com/MAnitosik/archlinux-post-install-scripts/refs/heads/main/etc/pacman.conf | sudo tee /etc/pacman.conf
+omarchy-channel-set edge
 sudo pacman -Rns lazygit lazydocker btop obsidian localsend libreoffice-fresh signal-desktop kdenlive 1password-beta 1password-cli typora spotify
-sudo pacman -Syu --needed flatpak
+sudo pacman -S --needed flatpak
 
 # required for the sysctl config (98-arch.conf)
 sudo modprobe tcp_bbr
