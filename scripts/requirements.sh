b8dd73cae561f3fd91c5a0a629d3e8ab18da8b15
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2025-12-31 18:34:40 +0300

    Update requirements.sh

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -5,13 +5,14 @@ sudo mkdir /etc/scx_loader # sched-ext
 sudo mkdir /etc/systemd/resolved.conf.d
 sudo mkdir /etc/udev/rules.d
 
-# deleting some base omarchy packages, installing flatpak and fully updating the system
+# changing the update channel to the stable branch, deleting some base omarchy packages, installing flatpak and fully updating the system
 # lazygit and lazydocker are not needed, obsidian is just a smart note taking app, localsend is useless for me
 # 1password, typora and spotify are cringe
 # https://learn.omacom.io/2/the-omarchy-manual
 sudo pacman -Rns lazygit lazydocker btop obsidian localsend libreoffice-fresh signal-desktop kdenlive 1password-beta 1password-cli typora spotify
-sudo pacman -S --needed flatpak
+omarchy-channel-set stable
 sudo pacman -Syuu
+sudo pacman -S --needed flatpak
 
 # required for the sysctl config (98-arch.conf)
 sudo modprobe tcp_bbr
